6c76a9ab7575a2e4ff15c46d746da5ca8378f8ba
lalitroshankhede <lalitroshankhede99@gmail.com>
2023-05-22 00:40:29 +0530

    changes in Loops.java file

---
+++ src/main/java/com/Assignment1/Canteen.java
@@ -0,0 +1,48 @@
+package com.Assignment1;
+
+import java.util.Scanner;
+
+public class Canteen {
+    public static void main(String[] args) {
+
+        System.out.println("************ Welcome To South Indian Food ****************");
+        System.out.println("Choose Item");
+        System.out.println(" 1.Masala Dosa \n 2.Sambhar Vada \n 3.Idli");
+        Scanner input=new Scanner(System.in);
+        int item=input.nextInt();
+         if (item==1)
+         {
+             System.out.println("Masala Dosa Rs. 30");
+
+         } else if (item==2) {
+
+             System.out.println("1.Full Plate 2.Half Plate");
+             int item2=input.nextInt();
+             if (item2==1)
+             {
+                 System.out.println("Sambhar Vada Full Plate Rs. 30");
+             }
+             else {
+                 System.out.println("Sambhar Vada Half Plate Rs. 20 ");
+             }
+         }
+
+         else if (item==3) {
+
+             System.out.println("1.Full Plate 2.Half Plate");
+             int item3=input.nextInt();
+             if (item3==1)
+             {
+                 System.out.println("Idli Full Plate Rs. 30");
+             }
+             else {
+                 System.out.println("Idli Half Plate Rs. 15 ");
+             }
+         }
+
+         else {
+             System.out.println("Invalid Option");
+         }
+
+    }
+}
