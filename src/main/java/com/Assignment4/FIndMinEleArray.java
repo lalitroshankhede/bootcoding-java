8b1e618fddb41737f059220e7a238b0067309382
lalitroshankhede <lalitroshankhede99@gmail.com>
2023-05-23 09:37:09 +0530

    created Assignment 4 java programs

---
+++ src/main/java/com/Assignment4/FIndMinEleArray.java
@@ -0,0 +1,16 @@
+package com.Assignment4;
+
+public class FIndMinEleArray {
+    public static void main(String[] args) {
+
+        int arr[] = {5, 0, 92, 1, 62};
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum element in array is :" + min);
+    }
+}
