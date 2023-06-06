999e5c992cd983663c36b3ddb9c6ff035dc8cad5
lalitroshankhede <lalitroshankhede99@gmail.com>
2023-06-06 11:55:03 +0530

    Number Pattern Triangle Program

---
+++ src/main/java/com/Patteren/TrianglePatternNumber.java
@@ -0,0 +1,20 @@
+package com.Patteren;
+
+public class TrianglePatternNumber {
+    public static void main(String[] args) {
+        int count=1;
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(count+" ");
+                count++;
+            }
+
+            System.out.println();
+
+        }
+    }
+}
