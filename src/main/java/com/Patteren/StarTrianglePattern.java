27d67eac5d6caf10eb1aa4cdaf1363debc158a02
lalitroshankhede <lalitroshankhede99@gmail.com>
2023-06-06 11:47:48 +0530

    Pattern Star Triangle and Diamond Program

---
+++ src/main/java/com/Patteren/StarTrianglePattern.java
@@ -0,0 +1,17 @@
+package com.Patteren;
+
+public class StarTrianglePattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
