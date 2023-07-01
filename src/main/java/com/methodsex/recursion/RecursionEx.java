3fff41915b99137ca5066db29922c68281d63e96
lalitroshankhede <lalitroshankhede99@gmail.com>
2023-07-01 16:26:04 +0530

    factorial program using Recursion in java

---
+++ src/main/java/com/methodsex/recursion/RecursionEx.java
@@ -0,0 +1,22 @@
+package com.methodsex.recursion;
+
+public class RecursionEx {
+
+    static int factorial(int n)
+    {
+        if (n==0 || n==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n*factorial(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int num=5;
+        System.out.println("Factorial of no is :"+factorial(num));
+    }
+}
