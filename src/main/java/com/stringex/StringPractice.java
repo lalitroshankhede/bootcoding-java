aad3174d0363a1b83fe903542759f2f4bb117361
lalitroshankhede <lalitroshankhede99@gmail.com>
2023-07-05 20:41:39 +0530

    String equals, == operator and compareTo method Example

---
+++ src/main/java/com/stringex/StringPractice.java
@@ -0,0 +1,67 @@
+package com.stringex;
+
+public class StringPractice {
+
+
+    //Note : The java.lang.String class implements Serializable, Comparable and CharSequence interfaces.
+
+    public static void main(String[] args) {
+
+//        char[] ch={'j','a','v','a','t','p','o','i','n','t'};
+//        String s=new String(ch);
+//        System.out.println(s);
+
+
+//        String str="Sachin";
+//       str= str.concat("Tendulkar");
+//        System.out.println(str);
+
+
+        //Equals Method
+//        String s1="Sachin";
+//        String s2="Sachin";
+//        String s3=new String("Sachin");
+//        String s4="Saurav";
+//        System.out.println(s1.equals(s2)); //true
+//        System.out.println(s1.equals(s3)); //true
+//        System.out.println(s1.equals(s4)); //false
+
+
+//        String s1="Sachin";
+//        String s2="SACHIN";
+//
+//        System.out.println(s1.equals(s2)); //true
+//        System.out.println(s1.equalsIgnoreCase(s2)); //false
+
+
+          // == operator
+//        String str1="Sachin";
+//        String str2="Sachin";
+//        String str3=new String("Sachin");
+//        System.out.println(str1==str2); // true
+//        System.out.println(str1==str3); // false
+//        System.out.println(str2==str3); // false
+
+        //compareTo method
+
+//        String str1="Lalit";
+//        String str2="Lalit";
+//        String str3=new String("Lalit");
+//        String str4=new String("Lalit");
+//
+//        System.out.println(str1==str2); //
+//        System.out.println(str1==str3); //
+//        System.out.println(str2==str3); //
+//        System.out.println(str3==str4); //
+
+
+//        String str1=new String("Lalit");
+//        String str2=new String(" Roshankhede");
+//        String str3=String.join("",str1,str2);  //String.join mehtod introduce in Java 8+ version
+//        System.out.println(str3);
+
+
+
+
+    }
+}
